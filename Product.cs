d214e3b203b4f0ca2a5d459e834a72bebd5f7177
SENEOArgb <162048770+SENEOArgb@users.noreply.github.com>
2024-10-31 00:58:40 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ARM_App.Model;
+
+public partial class Product : INotifyPropertyChanged
+{
+    public int productId { get; set; }
+
+    private string productName { get; set; } = null!;
+
+    public string ProductName
+    {
+        get { return productName; }
+        set
+        {
+            productName = value;
+            OnPropertyChanged("ProductName");
+        }
+    }
+
+    private string? productCategory { get; set; }
+
+    public string? ProductCategory
+    {
+        get { return productCategory; }
+        set
+        {
+            productCategory = value;
+            OnPropertyChanged("ProductCategory");
+        }
+    }
+
+    private decimal productSalary { get; set; }
+
+    public decimal ProductSalary
+    {
+        get { return productSalary; }
+        set
+        {
+            productSalary = value;
+            OnPropertyChanged("ProductSalary");
+        }
+    }
+
+    private DateOnly? expirationDate { get; set; }
+
+    public DateOnly? ExpirationDate
+    {
+        get { return expirationDate; }
+        set
+        {
+            expirationDate = value;
+            OnPropertyChanged("ExpirationDate");
+        }
+    }
+
+    private int? countProduct { get; set; }
+
+    public int? CountProduct
+    {
+        get { return countProduct; }
+        set
+        {
+            countProduct = value;
+            OnPropertyChanged("CountProduct");
+        }
+    }
+
+    public Product() { }
+
+    public Product(int productId, string productName, string? productCategory, decimal productSalary, DateOnly? expirationDate, int? countProduct)
+    {
+        this.productId = productId;
+        this.ProductName = productName;
+        this.ProductCategory = productCategory;
+        this.ProductSalary = productSalary;
+        this.ExpirationDate = expirationDate;
+        this.CountProduct = countProduct;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ RawMaterial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ARM_App.Model;
+
+public partial class RawMaterial : INotifyPropertyChanged
+{
+    public int rawMaterialId { get; set; }
+
+    private string nameRawMaterial { get; set; } = null!;
+
+    public string NameRawMaterial
+    {
+        get { return nameRawMaterial; }
+        set
+        {
+            nameRawMaterial = value;
+            OnPropertyChanged("NameRawMaterial");
+        }
+    }
+
+    private string? typeRawMaterial { get; set; }
+
+    public string? TypeRawMaterial
+    {
+        get { return typeRawMaterial; }
+        set
+        {
+            typeRawMaterial = value;
+            OnPropertyChanged("TypeRawMaterial");
+        }
+    }
+
+    private float? countRawMaterial { get; set; }
+
+    public float? CountRawMaterial
+    {
+        get { return countRawMaterial; }
+        set
+        {
+            countRawMaterial = value;
+            OnPropertyChanged("CountRawMaterial");
+        }
+    }
+
+    public RawMaterial() { }
+
+    public RawMaterial(int rawMaterialId, string nameRawMaterial, string? typeRawMaterial, float countRawMaterial)
+    {
+        this.rawMaterialId = rawMaterialId;
+        this.NameRawMaterial = nameRawMaterial;
+        this.TypeRawMaterial = typeRawMaterial;
+        this.CountRawMaterial = countRawMaterial;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
