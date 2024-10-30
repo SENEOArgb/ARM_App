746d50a8fab8c3a8559ebe6435b0de05b53c9fea
SENEOArgb <162048770+SENEOArgb@users.noreply.github.com>
2024-10-31 00:57:35 +0300

    Add files via upload

---
+++ WindowNewProduct.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ARM_App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARM_App.WindowNew
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowNewProduct.xaml
+    /// </summary>
+    public partial class WindowNewProduct : Window
+    {
+        public WindowNewProduct()
+        {
+            InitializeComponent();
+
+            DataContext = new ProductsViewModel();
+        }
+
+        private void BtSave_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void ExpDate_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (ExpDate.Visibility == Visibility.Hidden)
+            {
+                DpExpDate.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                DpExpDate.Visibility = Visibility.Hidden;
+            }
+        }
+
+    }
+}
