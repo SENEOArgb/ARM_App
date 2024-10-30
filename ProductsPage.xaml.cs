4b05c36c16fdb024fd54c6bfe7b0ebc072c3820b
SENEOArgb <162048770+SENEOArgb@users.noreply.github.com>
2024-10-31 00:58:07 +0300

    Add files via upload

---
+++ ProductsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ARM_App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ARM_App.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductsPage.xaml
+    /// </summary>
+    public partial class ProductsPage : Page
+    {
+        public ProductsPage()
+        {
+            InitializeComponent();
+            DataContext = new ProductsViewModel();
+        }
+    }
+}
---
+++ RawMaterialsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ARM_App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ARM_App.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RawMaterialsPage.xaml
+    /// </summary>
+    public partial class RawMaterialsPage : Page
+    {
+        public RawMaterialsPage()
+        {
+            InitializeComponent();
+
+            DataContext = new RawMaterialsViewModel();
+        }
+    }
+}
